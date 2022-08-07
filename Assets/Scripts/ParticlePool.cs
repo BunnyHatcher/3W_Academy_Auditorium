256541236caa188306d6db0132db6ef3f41a3266
BunnyHatcher <martinengelhaus@gmx.de>
2022-08-07 18:44:31 +0200

    minor changes

---
+++ Assets/Scripts/ParticlePool.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlePool : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _particlePrefab;
+    [SerializeField] private int _amountOfParticles;
+
+    private 
+
+
+
+
+}
